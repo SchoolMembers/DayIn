4df81572156c0ac609775b6dcf1e6b5ed061b5ff
Yujin Jeong <wjddbwls4297@naver.com>
2024-07-17 01:17:45 +0900

    [update] 캘린더 viewpager2 적용 완료 최종본

---
+++ app/src/main/java/com/example/dayin/CalendarFragment.kt
@@ -0,0 +1,64 @@
+package com.example.dayin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.time.LocalDate
+import java.time.YearMonth
+
+class CalendarFragment : Fragment() {
+
+    private lateinit var date: LocalDate
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            date = it.getSerializable(ARG_DATE) as LocalDate
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_calendar, container, false)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = GridLayoutManager(context, 7)
+        recyclerView.adapter = CalendarAdapter(dayInMonthArray(date))
+        return view
+    }
+
+    private fun dayInMonthArray(date: LocalDate): ArrayList<String> {
+        val dayList = ArrayList<String>()
+        val yearMonth = YearMonth.from(date)
+        val lastDay = yearMonth.lengthOfMonth()
+        val firstDay = date.withDayOfMonth(1)
+        val dayOfWeek = firstDay.dayOfWeek.value
+
+        for (i in 1..42) {
+            if (i <= dayOfWeek || i > (lastDay + dayOfWeek)) {
+                dayList.add("")
+            } else {
+                dayList.add((i - dayOfWeek).toString())
+            }
+        }
+        return dayList
+    }
+
+    companion object {
+        private const val ARG_DATE = "date"
+
+        @JvmStatic
+        fun newInstance(date: LocalDate) =
+            CalendarFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(ARG_DATE, date)
+                }
+            }
+    }
+}
+
